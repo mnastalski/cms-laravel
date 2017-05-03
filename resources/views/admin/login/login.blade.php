4981a27515a34278a9b6140b12adef855ecddb6c
mnastalski <kkzzit@gmail.com>
2017-05-03 14:37:55 +0200

    Updated CSS

--- resources/views/admin/login/login.blade.php
+++ resources/views/admin/login/login.blade.php
@@ -12,20 +12,20 @@
     <div class="body-half-2">
     </div>
 
-    <div class="container-fluid" style="position: absolute; top: 27%; width: 100%">
+    <div class="container-fluid login-container">
         <div class="row justify-content-center">
             <div class="col-10 col-sm-7 col-md-5 col-lg-4 col-xl-3 text-center text-uppercase login-box">
 
                 <form>
-                    <a href="/admin"><img src="{{ asset('img/logo_admin.png') }}" class="img-fluid" style="margin: 10px 0"></a>
+                    <a href="/admin"><img src="{{ asset('img/logo_admin.png') }}" class="img-fluid login-logo" style="height: 144px; width: 218px"></a>
 
                     <div class="input-group">
                         <span class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></span>
-                        <input type="text" class="form-control" placeholder="username" autocomplete="off">
+                        <input type="text" name="username" class="form-control" placeholder="username" autocomplete="off">
                     </div>
                     <div class="input-group">
                         <span class="input-group-addon"><i class="fa fa-lock" aria-hidden="true"></i></span>
-                        <input type="password" class="form-control" placeholder="password" autocomplete="off">
+                        <input type="password" name="password" class="form-control" placeholder="password" autocomplete="off">
                     </div>
 
                     <button type="submit" class="btn btn-primary">Login</button>
@@ -34,4 +34,11 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('js')
+    <script type="text/javascript">
+        $('.login-box').hide().fadeIn(900);
+        $('input[name="username"]').focus();
+    </script>
 @endsection
